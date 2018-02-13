4a14a24dd24f3ab16e00f439a0d0715a6d281a4c
Sergey Khmelevskoy <xs290@me.com>
2018-02-13 14:46:12 +0300

    added index pages

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,10 @@ let gulp 		= require('gulp'),
 	spritesmith = require('gulp.spritesmith'),
 	flexbugs    = require('postcss-flexbugs-fixes'),
 	postcss     = require('gulp-postcss');
-	
+
+var consolidate = require('gulp-consolidate');
+var yaml        = require('require-yaml');
+
 gulp.task('sass', function () {
 	return gulp.src('src/sass/**/*.+(sass|scss)')
 		.pipe(sass().on('error', sass.logError))
@@ -56,7 +59,7 @@ gulp.task('compressImages', function () {
 });
 
 gulp.task('sprite', function() {
-    var spriteData = 
+    var spriteData =
         gulp.src('src/img/sprite/*.*') // путь, откуда берем картинки для спрайта
             .pipe(spritesmith({
                 imgName: 'sprite.png',
@@ -65,7 +68,7 @@ gulp.task('sprite', function() {
                 algorithm: 'binary-tree', // алгоритм сортировки иконок в спрайте
                 imgPath: 'img/sprite.png', // путь до спрайта
                 padding: 1,
-				cssTemplate: 'src/sass/sass.template.mustache', 
+				cssTemplate: 'src/sass/sass.template.mustache',
 				cssVarMap: function(sprite) {
                     sprite.name = 's-' + sprite.name
                 }
@@ -73,6 +76,17 @@ gulp.task('sprite', function() {
 
     spriteData.img.pipe(gulp.dest('src/img/')); // путь, куда сохраняем картинку
     spriteData.css.pipe(gulp.dest('src/sass/')); // путь, куда сохраняем стили
+});
+
+gulp.task('list-pages', function() {
+	delete require.cache[require.resolve('./src/list-pages/index.yaml')]
+  var pages = require('./src/list-pages/index.yaml');
+  return gulp
+    .src('src/list-pages/index.html')
+    .pipe(consolidate('lodash', {
+      pages: pages
+    }))
+    .pipe(gulp.dest('src'));
 });
 
 gulp.task('liveReload', function () {
@@ -84,8 +98,9 @@ gulp.task('liveReload', function () {
 	});
 });
 
-gulp.task('watch', ['liveReload', 'sass', 'css-libs', 'scripts', 'sprite'], function () {
+gulp.task('watch', ['liveReload', 'sass', 'css-libs', 'scripts', 'sprite', 'list-pages'], function () {
 	gulp.watch('src/sass/**/*.+(sass|scss)', ['sass']);
+	gulp.watch('./src/list-pages/index.yaml', ['list-pages']);
 	gulp.watch('src/*.html', browserSync.reload);
 	gulp.watch('src/js/*.js', browserSync.reload);
 });
@@ -94,22 +109,21 @@ gulp.task('clean', function () {
 	return del.sync('dist');
 });
 
-gulp.task('build', ['clean', 'sass', 'css-libs', 'scripts', 'compressImages', 'sprite'], function () {
+gulp.task('build', ['clean', 'sass', 'css-libs', 'scripts', 'compressImages', 'sprite', 'list-pages'], function () {
 	gulp.src('src/*.html')
 		.pipe(gulp.dest('dist'));
-		
+
 	gulp.src(['src/img/**/*', '!src/img/sprite/*'])
-		.pipe(gulp.dest('dist/img'));		
-		
+		.pipe(gulp.dest('dist/img'));
+
 	gulp.src('src/fonts/**/*')
 		.pipe(gulp.dest('dist/fonts'));
-		
+
 	gulp.src('src/css/**/*.css')
-		.pipe(gulp.dest('dist/css'));		
-		
+		.pipe(gulp.dest('dist/css'));
+
 	gulp.src('src/js/**/*.js')
-		.pipe(gulp.dest('dist/js'));	
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('default', ['watch']);
-
